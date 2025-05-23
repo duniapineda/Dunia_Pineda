8bc3533e704bfa80e514326a228f88b2e8766bf3
duniapineda <duniaacosta62@gmail.com>
2025-05-23 12:33:45 -0600

    Add files via upload

---
+++ c#28/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace C_28
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.picImagen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(561, 89);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(82, 37);
+            this.btnCargar.TabIndex = 0;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // picImagen
+            // 
+            this.picImagen.Image = global::C_28.Properties.Resources._2;
+            this.picImagen.Location = new System.Drawing.Point(336, 102);
+            this.picImagen.Name = "picImagen";
+            this.picImagen.Size = new System.Drawing.Size(166, 149);
+            this.picImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picImagen.TabIndex = 1;
+            this.picImagen.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picImagen);
+            this.Controls.Add(this.btnCargar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picImagen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.PictureBox picImagen;
+    }
+}
+
